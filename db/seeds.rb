23539b41bacafb96e403788ebed79b373bf6da48
guillermo varela <guillovarela16@gmail.com>
2019-03-22 11:09:43 -0300

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Mueble.destroy_all
+
+puts 'Creating muebles...'
+Mueble.create!(sku: 'co-001', name: 'comoda', category: 'habitacion', photo_url: 'https://cdn1.mueblesrey.com/2832-large_default/mueble-para-tv-delta.jpg')
+Mueble.create!(sku: 'co-002', name: 'mesa', category: 'living', photo_url: 'https://cdn.jysk.es/media/catalog/product/cache/9/thumbnail/960x/d34aaef3401395db2614ec4ce2acbbee/3/6/36511001_1__1.jpg')
+puts 'Finished!'

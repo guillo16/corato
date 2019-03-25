21cd949983810bd80e468e3400375325a1910213
guillermo varela <guillovarela16@gmail.com>
2019-03-25 10:25:30 -0300

    order controller finished

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,9 @@
 class OrdersController < ApplicationController
+
+  def create
+    mueble = Mueble.find(params[:mueble_id])
+    order  = Order.create!(mueble_sku: mueble.sku, amount: mueble.price, state: 'pending', user: current_user)
+
+    redirect_to new_order_payment_path(order)
+  end
 end
